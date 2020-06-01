21b7c0403dc06b0a6ed839acfbc819fb829eb83e
Luis Oliveira <dev.oliveiras@gmail.com>
2020-05-31 21:48:26 -0300

    Added project base code

---
+++ Editor/ScriptableStateEditor.cs
@@ -0,0 +1,39 @@
+﻿using Malee.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.TerrainAPI;
+using UnityEngine;
+
+[CustomEditor(typeof(ScriptableState))]
+public class ScriptableStateEditor : Editor
+{
+    ReorderableList entryActions;
+    ReorderableList stateActions;
+    ReorderableList exitActions;
+    ReorderableList transitions;
+
+    private void OnEnable()
+    {
+        entryActions = new ReorderableList(serializedObject.FindProperty("_entryActions"));
+        entryActions.elementLabels = false;
+        stateActions = new ReorderableList(serializedObject.FindProperty("_stateActions"));
+        stateActions.elementLabels = false;
+        exitActions = new ReorderableList(serializedObject.FindProperty("_exitActions"));
+        exitActions.elementLabels = false;
+        transitions = new ReorderableList(serializedObject.FindProperty("_transitions"));
+        transitions.elementNameOverride = "Transition";
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        entryActions.DoLayoutList();
+        stateActions.DoLayoutList();
+        exitActions.DoLayoutList();
+        transitions.DoLayoutList();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Runtime/StateTransition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct StateTransition
+{
+    public ScriptableCondition condition;
+    public ScriptableState trueState;
+    public ScriptableState falseState;
+}
