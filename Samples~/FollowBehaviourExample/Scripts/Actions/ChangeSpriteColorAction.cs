e9489b1b851f905c5c97d17abbf4a03b8c0cf7d3
Luis Oliveira <dev.oliveiras@gmail.com>
2020-08-30 21:24:05 -0300

    Added Sample 2 - Follow Behaviour Example - to the package

---
+++ Samples~/FollowBehaviourExample/Scripts/Actions/ChangeSpriteColorAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using devludico.ScriptableStates;
+
+[CreateAssetMenu(menuName = "Scriptable State Machine/Actions/ChangeSpriteColorAction", fileName = "new ChangeSpriteColorAction")]
+public class ChangeSpriteColorAction : ScriptableAction
+{
+	public Color newColor = Color.white;
+
+	public override void Act(ScriptableStatesComponent statesComponent)
+	{
+		statesComponent.GetComponent<SpriteRenderer>().color = newColor;
+	}
+}
---
+++ Samples~/FollowBehaviourExample/Scripts/Actions/ConsumeStaminaAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using devludico.ScriptableStates;
+
+[CreateAssetMenu(menuName = "Scriptable State Machine/Actions/ConsumeStaminaAction", fileName = "new ConsumeStaminaAction")]
+public class ConsumeStaminaAction : ScriptableAction
+{
+	public override void Act(ScriptableStatesComponent statesComponent)
+	{
+		statesComponent.GetComponent<StaminaComponent>().ConsumeStamina();
+	}
+}
